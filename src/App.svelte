86020687345e86f2255c230df57cabeb681da582
Beslan <64842276+Beslan066@users.noreply.github.com>
2021-09-07 16:40:40 +0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Router from 'svelte-spa-router';
+	import Signin from './routes/Signin.svelte';
+	import Contracts from './routes/ Contracts.svelte'
+    import Home from "./routes/Home.svelte";
+	import Registry from './routes/Registry.svelte';
+</script>
+
+<Router routes={{
+    '/': Home,
+	'/signin/': Signin,
+	'/registry/': Registry,
+	'/cotractpage/': Contracts
+}} />
+
+<style>
+	
+
+
+</style>
---
+++ src/routes/Home.svelte
@@ -0,0 +1,40 @@
+<script>
+    import Header from '../components/Header.svelte';
+    import Contracts from './ Contracts.svelte'
+    import { Col, Container, Row } from 'sveltestrap';
+
+</script>
+
+<svelte:head>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css">
+</svelte:head>
+
+    <Header />
+
+
+
+
+
+
+
+
+
+<style>
+    
+
+</style>
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
